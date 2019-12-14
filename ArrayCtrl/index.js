df4befa968d71859d54a180db9098ef8d0140bbd
李铭辉 <1018596741@qq.com>
2019-12-14 10:44:51 +0800

    重构结构，使其模块化

---
+++ ArrayCtrl/index.js
@@ -0,0 +1,4 @@
+import { ArrayCtrl, differArray, objectCheck, objectEqual, inventObject, statusSwitch, hasArray } from './ArrayObject/index';
+import { Node, DoubleLinkList, Stack } from './dataStructure/index';
+export { ArrayCtrl, differArray, objectCheck, objectEqual, inventObject, statusSwitch, hasArray, DoubleLinkList, Node, Stack };
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { ArrayCtrl, differArray, objectCheck, objectEqual, inventObject, statusSwitch, hasArray } from './ArrayObject/index'
+import { Node, DoubleLinkList, Stack } from './dataStructure/index'
+
+
+export { 
+    ArrayCtrl,
+     differArray, 
+     objectCheck, objectEqual, 
+     inventObject, 
+     statusSwitch, 
+     hasArray, 
+     DoubleLinkList, 
+     Node, 
+     Stack}
