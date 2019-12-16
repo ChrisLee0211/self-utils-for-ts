40c206441103fca3cbc8d8a93a5f4ac95607a447
李铭辉 <1018596741@qq.com>
2019-12-16 19:45:02 +0800

    新增对象数组排序方法

--- ArrayCtrl/ArrayObject/index.js
+++ ArrayCtrl/ArrayObject/index.js
@@ -237,4 +237,47 @@ export var objectEqual = function (x, y) {
     }
     return true;
 };
+export var sortObject = function (arr, target, sort) {
+    var _a;
+    var result;
+    var targetArr = __spreadArrays(arr);
+    //判断是否数组
+    if (targetArr instanceof Array) {
+        //判断数组是否为空
+        if (targetArr.length < 1) {
+            console.error('数组为空！');
+            result = arr;
+        }
+        else {
+            //判断数组中每一项是否对象
+            var isObj = targetArr.every(function (v) { return v instanceof Object; });
+            //判断传入的属性是否在数组的每个对象中，且为可排序类型
+            var isRealKey = targetArr.every(function (v) {
+                var keysArr = Object.keys(v);
+                return keysArr.includes(target);
+            });
+            if (isObj && isRealKey) {
+                var len = targetArr.length;
+                for (var i = 0; i < len - 1; i++) {
+                    var min = i;
+                    for (var j = i + 1; j < len; j++) {
+                        if (targetArr[j].target < targetArr[min].target) {
+                            min = j;
+                        }
+                        _a = [targetArr[min], targetArr[i]], targetArr[i] = _a[0], targetArr[min] = _a[1];
+                    }
+                }
+                result = __spreadArrays(targetArr);
+            }
+            else {
+                console.error('传入的目标属性无法在每个对象中找到');
+                result = arr;
+            }
+        }
+    }
+    else {
+        result = arr;
+    }
+    return result;
+};
 //# sourceMappingURL=index.js.map
