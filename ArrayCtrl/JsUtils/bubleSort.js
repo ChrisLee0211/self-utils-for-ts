b307868e95a1a458666e012143e913f3f8c974d2
李铭辉 <1018596741@qq.com>
2020-10-16 22:45:32 +0800

    feat: 冒泡排序

---
+++ ArrayCtrl/JsUtils/bubleSort.js
@@ -0,0 +1,27 @@
+"use strict";
+/**
+ * 冒泡排序
+ */
+function bubble(arr) {
+    var _a;
+    if (arr.length < 1)
+        return arr;
+    var len = arr.length;
+    var flag = false;
+    while (len > 0) {
+        for (var i = 0; i < len; i++) {
+            var j = i + 1;
+            if (j < len && arr[i] > arr[j]) {
+                _a = [arr[j], arr[i]], arr[i] = _a[0], arr[j] = _a[1];
+                flag = true;
+            }
+        }
+        len--;
+        if (flag === false) {
+            break;
+        }
+    }
+    ;
+    return arr;
+}
+//# sourceMappingURL=bubleSort.js.map
---
+++ src/JsUtils/bubleSort.ts
@@ -0,0 +1,23 @@
+/**
+ * 冒泡排序
+ */
+function bubble(arr:Array<number>):Array<number>{
+    if(arr.length<1)return arr;
+    let len:number = arr.length;
+    let flag:boolean = false;
+    while(len>0){
+        for(let i=0;i<len;i++){
+            let j = i+1;
+            if(j<len&&arr[i]>arr[j]){
+                [arr[i],arr[j]] = [arr[j],arr[i]];
+                flag = true;
+            }
+            
+        }
+        len--;
+        if(flag === false){
+            break
+        }
+    };
+    return arr
+}
