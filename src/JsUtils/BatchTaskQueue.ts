01b5c677aaa70328cbdead38ccc5cd177a131091
李铭辉 <1018596741@qq.com>
2022-02-08 23:07:34 +0800

    feat: 缓冲队列增加时间切片

--- src/JsUtils/BatchTaskQueue.ts
+++ src/JsUtils/BatchTaskQueue.ts
@@ -19,11 +19,13 @@ interface BatchTaskQueue<T = any> {
 
 class BatchTaskQueueCtreator<T> implements BatchTaskQueue {
     private queue:T[] = [];
-    private delay:number = 1500
+    private delay:number = 1500;
+    private batchLimit:number = 0;
     private flushQueueFn:null | ((cb:batchTaskFn<T>) => void) = null
 
-    constructor(delay:number) {
+    constructor(delay:number, limit = 0) {
         this.delay = delay;
+        this.batchLimit = limit;
         this.flushQueueFn = this.flushTaskCreator();
         this.push = this.push.bind(this);
         this.destory = this.destory.bind(this);
@@ -35,13 +37,49 @@ class BatchTaskQueueCtreator<T> implements BatchTaskQueue {
             this.flushQueueFn(cb);
         }
     }
+    
+    private timeSlice(gen:GeneratorFunction):Function|void{
+        if(typeof gen!=="function") throw new Error(`TypeError: the param expect a generator function`)
+        const g = gen();
+        if(!g||typeof g.next !== "function") return;
+        return function next(){
+            const start = performance.now();
+            let res = null;
+            do{
+                res = g.next()
+            }while(res.done!==true&&performance.now()-start>25);
+            if(res.done) return
+            setTimeout(next)
+        }
+    }
+
+    private gen(items:T[], limit:number,cbFn:batchTaskFn<T>){
+        return function*() {
+            const taskItems = items;
+            while(taskItems.length) {
+                const slice = [];
+                const len = Math.min(limit,taskItems.length);
+                for(let i =0;i< len; i++) {
+                    const cur = taskItems.shift();
+                    if (cur) {
+                        slice.push(cur);
+                    }
+                };
+                yield cbFn(slice)
+            }
+        }
+    }
 
     private flushTaskCreator() {
         return throttleFn((cb:batchTaskFn<T>) => {
             const prsentQueue = this.queue.slice();
             const cbFn = cb.bind(cb);
-            this.queue = [];
-            cbFn(prsentQueue);
+            if (this.batchLimit === 0) {
+                this.queue = [];
+                cbFn(prsentQueue);
+            } else {
+                const timeSlicer = this.timeSlice(this.gen(prsentQueue,this.batchLimit,cbFn))
+            }
         }, this.delay);
     }
 
